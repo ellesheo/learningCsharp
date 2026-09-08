d41ed9f5a2b7b4b2d01c4a78d6449c50e0662318
Алина <@DESKTOP-4GD3CN4>
2026-09-08 14:09:39 +0300

    Добавьте файлы проекта.

---
+++ CP3_peregruzka_operatorov/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CP3_peregruzka_operatorov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
